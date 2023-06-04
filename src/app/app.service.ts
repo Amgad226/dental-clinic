21b7f5d71974ee24e0c8f6b769a501be61b575f9
Amgad226 <amgad.wr.1@gmail.com>
2023-06-04 18:41:53 +0300

    fix connect to mysql inside container | add npm command for up and down docker compose

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -4,6 +4,6 @@ import {  Injectable } from "@nestjs/common";
 export class AppService{
 
     helloWorld(){
-        return 'Hello, World-this change fordm local122';
+        return 'Hello, World-this change fordm local1221';
     }
 }
