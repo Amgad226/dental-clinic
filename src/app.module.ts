e92df7bff8bf571042a790ea01733838d5dc06fe
@Ayham_Hammami <ayhmhamame14@gmail.com>
2023-07-19 22:50:33 +0300

    conf (app) : edit some files config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,26 +1,26 @@
-import { Module } from '@nestjs/common';
-
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { PrismaModule } from './prisma/prisma.module';
-import { GraphQLModule } from '@nestjs/graphql';
-import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { join } from 'path';
-// import { DiseasModule } from './diseas/diseas.module';
+import { Module } from '@nestjs/common';
+import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { AppController } from './app.controller';
+import { PrismaModule } from './prisma/prisma.module';
 import { DiseaseModule } from './graphql/disease/disease.module';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { BadHabitModule } from './graphql/bad_habit/bad_habit.module';
 import { TreatmentTypeModule } from './graphql/treatment_type/treatment_type.module';
+import { ProblemTypeModule } from './graphql/problem_type/problem_type.module';
+import { ProblemModule } from './problem/problem.module';
 
 
 @Module({
   imports: [
     PrismaModule,
     GraphQLModule.forRoot<ApolloDriverConfig>({
-      
-      formatError: (error:any) => {
+
+      formatError: (error: any) => {
         // console.log(error);
         const graphQLFormattedError = {
-          message: error.message|| "SERVER_ERROR",
+          message: error.message || "SERVER_ERROR",
           code: error.extensions?.code || error.code || 500,
           // path:error.path
           // name: error.extensions?.exception?.name || error.name || "name",
@@ -30,13 +30,15 @@ import { TreatmentTypeModule } from './graphql/treatment_type/treatment_type.mod
       driver: ApolloDriver,
       playground: true,
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
-      sortSchema: true,}),
+      sortSchema: true,
+    }),
     DiseaseModule,
     BadHabitModule,
     TreatmentTypeModule,
+    ProblemTypeModule,
+    ProblemModule
   ],
   controllers: [AppController],
-  providers:[AppService],
-  })
-export class AppModule {}
-// problems
+  providers: [AppService],
+})
+export class AppModule { }
