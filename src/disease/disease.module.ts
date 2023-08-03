6beab68c6cb17ac2c8bf7658b5300100e194b172
abd_alrahman <76664726+Abd-Alrahman-Alkateb@users.noreply.github.com>
2023-08-03 17:20:55 +0300

    delete old disease folder

--- src/disease/disease.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { DiseaseController } from './disease.controller';
-import { DiseaseService } from './disease.service';
-
-@Module({
-  controllers: [DiseaseController],
-  providers: [DiseaseService]
-})
-export class DiseaseModule {}
--- src/disease/dto/disease.dto.ts
+++
@@ -1,7 +0,0 @@
-import { IsString ,IsNotEmpty} from "class-validator";
-
-export class DiseaseDto {
-    @IsString()
-    @IsNotEmpty()
-    name :string;
-}
