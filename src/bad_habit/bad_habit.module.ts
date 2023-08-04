6543c073ffeec84b3a202d5152bda6258cda8563
abd_alrahman <76664726+Abd-Alrahman-Alkateb@users.noreply.github.com>
2023-08-04 15:25:11 +0300

    delete old badhabit crud

--- src/bad_habit/bad_habit.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { BadHabitController } from './bad_habit.controller';
-import { BadHabitService } from './bad_habit.service';
-
-@Module({
-  controllers: [BadHabitController],
-  providers: [BadHabitService]
-})
-export class BadHabitModule {}
