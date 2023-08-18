ff1741131587d8341f5ca7f574e43e14bdbc75aa
Amgad226 <amgad.wr.1@gmail.com>
2023-08-18 17:30:22 +0300

    fix (chemecal material) fix update conflicts chemecal material

--- src/problem/entities/PaginateProblem.ts
+++
@@ -1,6 +0,0 @@
-import { ObjectType } from '@nestjs/graphql';
-import { PaginateResult } from '../../pagination/PaginateResult';
-import { Problem } from './problem.entity';
-
-@ObjectType()
-export class PaginateProblem extends PaginateResult(Problem) {}
