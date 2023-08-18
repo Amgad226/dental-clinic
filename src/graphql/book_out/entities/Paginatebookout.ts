4a2c1f116a1a0989cfc394b9c7856a953cbaf2d7
abd_alrahman <76664726+Abd-Alrahman-Alkateb@users.noreply.github.com>
2023-08-18 16:02:34 +0300

    fix: delete old warehouse cruds + fix phpmyadmin port

--- src/graphql/book_out/entities/Paginatebookout.ts
+++
@@ -1,6 +0,0 @@
-import { ObjectType } from "@nestjs/graphql";
-import { PaginateResult } from "src/pagination/PaginateResult";
-import { BookOut } from "./book_out.entity";
-
-@ObjectType()
-export class Paginatebookout extends PaginateResult(BookOut){};
