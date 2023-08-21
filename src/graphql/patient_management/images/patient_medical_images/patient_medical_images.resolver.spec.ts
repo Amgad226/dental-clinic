1087cd4b77fbd67a0cd4251008fe06ec224f842d
abd_alrahman <76664726+Abd-Alrahman-Alkateb@users.noreply.github.com>
2023-08-21 19:36:25 +0300

    fix:(graphql/patient_management/images): update the test files to be tested

--- src/graphql/patient_management/images/patient_medical_images/patient_medical_images.resolver.spec.ts
+++ src/graphql/patient_management/images/patient_medical_images/patient_medical_images.resolver.spec.ts
@@ -1,13 +1,16 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { PrismaService } from '../../../../prisma/prisma.service';
+import { ImagesUploaderService } from '../../../../images_uploader/images_uploader.service';
 import { PatientMedicalImagesResolver } from './patient_medical_images.resolver';
 import { PatientMedicalImagesService } from './patient_medical_images.service';
+
 
 describe('PatientMedicalImagesResolver', () => {
   let resolver: PatientMedicalImagesResolver;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [PatientMedicalImagesResolver, PatientMedicalImagesService],
+      providers: [PatientMedicalImagesResolver, PatientMedicalImagesService, PrismaService, ImagesUploaderService],
     }).compile();
 
     resolver = module.get<PatientMedicalImagesResolver>(PatientMedicalImagesResolver);
