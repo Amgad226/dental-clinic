045d15099fdbb6d55d9b2584fa668dd85d36cf9b
abd_alrahman <76664726+Abd-Alrahman-Alkateb@users.noreply.github.com>
2023-08-23 21:08:45 +0300

    fix:(paient_lab_order.entity) : add more fileds to the file

--- src/graphql/patient_management/patient_lab_orders/entities/patient_lab_order.entity.ts
+++ src/graphql/patient_management/patient_lab_orders/entities/patient_lab_order.entity.ts
@@ -13,6 +13,24 @@ export class PatientLabOrder {
   @Field(() => Int)
   lab_order_id: number;
 
+  @Field(() => String)
+  type: string;
+
+  @Field(() => String)
+  degree: string;
+
+  @Field(() => String)
+  directions: string;
+
+  @Field(() => Date)
+  created_at: Date;
+
+  @Field(() => Date)
+  deliver_at: Date;
+
+  @Field(() => [String])
+  notation: string[];
+
   @Field(() => PatientSession)
   session: PatientSession;
   
